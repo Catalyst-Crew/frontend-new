e660f48be8983d2ff6b8364495dfd743f299abdd
Axole Maranjana <82832027+Axolem@users.noreply.github.com>
2023-08-09 02:13:56 +0200

    #17

--- src/renderer/src/screens/protected/Users.jsx
+++ src/renderer/src/screens/protected/Users.jsx
@@ -1,8 +1,6 @@
 import { Card } from 'primereact/card'
-import { Badge } from 'primereact/badge'
 import { Toast } from 'primereact/toast'
 import { Button } from 'primereact/button'
-import { Avatar } from 'primereact/avatar'
 import { Column } from 'primereact/column'
 import { Dropdown } from 'primereact/dropdown'
 import { DataTable } from 'primereact/datatable'
@@ -10,10 +8,12 @@ import { DataTable } from 'primereact/datatable'
 import Navbar from '../../components/Navbar'
 import { API_URL } from '../../utils/exports'
 import NewUser from '../../components/NewUser'
-import { catchHandler, showToast } from '../../utils/functions'
+import username from '../../components/UserName'
 import staticData from '../../assets/staticData.json'
+import { catchHandler, showToast } from '../../utils/functions'
 
 import axios from 'axios'
+import moment from 'moment'
 import { useSelector } from 'react-redux'
 import { useEffect, useRef, useState } from 'react'
 
@@ -21,22 +21,43 @@ const Users = () => {
   const toast = useRef(null)
   const { user } = useSelector((state) => state.auth)
 
-  const [users, setUsers] = useState([])
+  const [users, setUsers] = useState([
+    {
+      user_id: 1000009,
+      user_id_prefix: 'user-',
+      user_name: 'Test Supervisor',
+      role_id: 1000000,
+      role_name: 'Admin',
+      email: 'supervisor6@mail.com',
+      created_by: 'axole',
+      created_at: '2023-08-03T17:55:37.000Z',
+      updated_by: 'axole',
+      updated_at: '2023-08-03T17:55:37.000Z',
+      phone: '',
+      access_id: 1000000,
+      access_name: 'Granted',
+      area_id: 1000000
+    }
+  ])
   const [visible, setVisible] = useState(false)
   const [selectedAccess, setAccess] = useState(null)
   const [selectedAccessArea, setAccessArea] = useState(null)
   const [selectedAccessLevel, setAccessLevel] = useState(null)
   const [selectedUser, setUser] = useState({
-    name: '',
-    email: '',
-    access: '',
-    area: '',
-    access_level: '',
-    access_area: '',
-    last_updated: '',
-    updated_by: '',
-    created: '',
-    created_by: ''
+    user_id: 1000009,
+    user_id_prefix: 'user-',
+    user_name: 'Test Supervisor',
+    role_id: 1000000,
+    role_name: 'Admin',
+    email: 'supervisor6@mail.com',
+    created_by: 'axole',
+    created_at: '2023-08-03T17:55:37.000Z',
+    updated_by: 'axole',
+    updated_at: '2023-08-03T17:55:37.000Z',
+    phone: '',
+    access_id: 1000000,
+    access_name: 'Granted',
+    area_id: 1000000
   })
 
   useEffect(() => {
@@ -44,7 +65,7 @@ const Users = () => {
   }, [])
 
   const token = user ? user.token : 'token'
-  const name = user ? user.name : 'Admin'
+  const name = user ? user.id_prefix + user.id : 'user-999999'
 
   const fetchtUsers = () => {
     axios
@@ -55,9 +76,9 @@ const Users = () => {
         }
         setUser(res.data.data[0])
         setUsers(res.data.data)
-        setAccess(res.data.data[0].access)
-        setAccessArea(res.data.data[0].areaId)
-        setAccessLevel(res.data.data[0].role)
+        setAccess(res.data.data[0].access_id)
+        setAccessArea(res.data.data[0].access_id)
+        setAccessLevel(res.data.data[0].role_id)
       })
       .catch((err) => {
         catchHandler(err, toast)
@@ -73,7 +94,9 @@ const Users = () => {
     }
 
     axios
-      .put(`${API_URL}/users/${selectedUser.id}`, data, { headers: { 'x-access-token': token } })
+      .put(`${API_URL}/users/${selectedUser.user_id}`, data, {
+        headers: { 'x-access-token': token }
+      })
       .then((res) => {
         if (res.status !== 200) {
           return
@@ -95,11 +118,11 @@ const Users = () => {
   }
 
   const selectUser = (userData) => {
-    const user = users.find((user) => user.id === userData.id)
+    const user = users.find((user) => user.user_id === userData.user_id)
     setUser(user)
-    setAccess(user.access)
-    setAccessArea(user.areaId)
-    setAccessLevel(user.role)
+    setAccess(user.access_id)
+    setAccessArea(user.area_id)
+    setAccessLevel(user.role_id)
   }
 
   return (
@@ -107,24 +130,24 @@ const Users = () => {
       <Navbar activeIndex={1} />
       <Toast ref={toast} />
 
-      <div className="flex mt-3 h-max-h-full">
+      <div className="flex h-max-h-full">
         {/* Table div */}
         <div className="w-8 px-3 ">
           <DataTable
             value={users}
             scrollable
             className="max-h-full"
-            scrollHeight="calc(100vh - 15rem)"
+            scrollHeight="calc(100vh - 5rem)"
           >
             <Column
               header="Name"
-              filterField="name"
+              filterField="user_name"
               filter
               filterPlaceholder="Search by name"
-              body={imageBodyTemplate}
+              body={username}
             />
-            <Column field="access" sortable header="Access" />
-            <Column field="area" sortable header="Area" />
+            <Column field="access_name" sortable header="Access" />
+            <Column field="area_name" sortable header="Area" />
             <Column header="Action" body={actionBodyTemplate} />
           </DataTable>
         </div>
@@ -136,7 +159,7 @@ const Users = () => {
           </Button>
 
           <Card className="sidebar">
-            {imageBodyTemplate(selectedUser)}
+            {username(selectedUser)}
 
             <div className="mt-4">
               <div className="flex flex-column gap-2 text-left">
@@ -188,7 +211,9 @@ const Users = () => {
                           <p className="m-0 mt-4">Updated:</p>
                         </td>
                         <td>
-                          <p className="m-0  mt-4 font-bold">{selectedUser.last_updated}</p>
+                          <p className="m-0  mt-4 font-bold">
+                            {moment(selectedUser.updated_at).format('LLL')}
+                          </p>
                         </td>
                       </tr>
                       <tr>
@@ -205,7 +230,9 @@ const Users = () => {
                           <p className="m-0 mt-4">Created:</p>
                         </td>
                         <td>
-                          <p className="m-0  mt-4 font-bold">{selectedUser.created}</p>
+                          <p className="m-0  mt-4 font-bold">
+                            {moment(selectedUser.created_at).format('LLL')}
+                          </p>
                         </td>
                       </tr>
                       <tr>
@@ -233,35 +260,35 @@ const Users = () => {
 
 export default Users
 
-const imageBodyTemplate = (user) => {
-  let name = ''
+// const imageBodyTemplate = (user) => {
+//   let name = ''
 
-  if (user.name.includes(' ')) {
-    const nameArr = user.name.split(' ')
-    name = nameArr[0][0] + nameArr[1][0]
-  } else {
-    name = user.name.substring(0, 2)
-  }
+//   if (user.user_name.includes(' ')) {
+//     const nameArr = user.user_name.split(' ')
+//     name = nameArr[0][0] + nameArr[1][0]
+//   } else {
+//     name = user.user_name.substring(0, 2)
+//   }
 
-  return (
-    <div className="flex align-items-center">
-      <Avatar
-        className="p-overlay-badge"
-        label={name.toUpperCase()}
-        size="large"
-        style={{ backgroundColor: '#2196F3', color: '#ffffff' }}
-        shape="circle"
-      >
-        <Badge
-          severity={
-            user.access === 'granted' ? 'success' : user.access === 'blocked' ? 'warning' : 'danger'
-          }
-        />
-      </Avatar>
-      <div className="ml-2">
-        <p className="font-bold m-0 p-0">{user.name}</p>
-        <p className="text-sm m-0 p-0">{user.email}</p>
-      </div>
-    </div>
-  )
-}
+//   return (
+//     <div className="flex align-items-center">
+//       <Avatar
+//         className="p-overlay-badge"
+//         label={name.toUpperCase()}
+//         size="large"
+//         style={{ backgroundColor: '#2196F3', color: '#ffffff' }}
+//         shape="circle"
+//       >
+//         <Badge
+//           severity={
+//             user.access_name === 'Granted' ? 'success' : user.access === 'Blocked' ? 'warning' : 'danger'
+//           }
+//         />
+//       </Avatar>
+//       <div className="ml-2">
+//         <p className="font-bold m-0 p-0">{user.user_name}</p>
+//         <p className="text-sm m-0 p-0">{user.email}</p>
+//       </div>
+//     </div>
+//   )
+// }
