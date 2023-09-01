aad60bb4cbaa14c13a16d1f255839a0d80d25c4b
Axole Maranjana <82832027+Axolem@users.noreply.github.com>
2023-09-01 18:21:55 +0200

    Code refactoring

--- src/renderer/src/main.jsx
+++ src/renderer/src/main.jsx
@@ -1,15 +1,13 @@
 import App from './App'
-import { PrimeReactContext } from 'primereact/api'
 import { createRoot } from 'react-dom/client'
+import { PrimeReactContext } from 'primereact/api'
 //css
 import './assets/css/index.css'
 import './assets/css/theme.css'
-import 'primereact/resources/primereact.min.css'
-import 'primeflex/primeflex.css'
 
-//import "primereact/resources/themes/lara-light-indigo/theme.css";
-//import "primereact/resources/primereact.css";
+import 'primeflex/primeflex.css'
 import 'primeicons/primeicons.css'
+import 'primereact/resources/primereact.min.css'
 
 //Redux
 import store from './store/store'
