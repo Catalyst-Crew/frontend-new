e228a2178b93723a62f64a1eaff6bdd19432fb90
Axole Maranjana <82832027+Axolem@users.noreply.github.com>
2023-09-01 18:22:38 +0200

    Surround app with socket

--- src/renderer/src/App.jsx
+++ src/renderer/src/App.jsx
@@ -12,34 +12,39 @@ import Settings from './screens/protected/Settings'
 import Employee from './screens/protected/Employee'
 import ForgotPassword from './screens/ForgotPassword'
 import AccessPoints from './screens/protected/AccessPoints'
+import SocketDashboardWrapper from './components/SocketDashboardWrapper'
 
 export default function App() {
   const isLogged = useSelector((state) => state.auth.state)
   const user = useSelector((state) => state.auth.user)
 
   return (
-    <HashRouter>
-      {isLogged && user ? (
-        //true ? (
-        <Routes>
-          <Route path="/" index element={<Dashbord />} />
-          <Route path="/help" element={<Help />} />
-          <Route path="/logs" element={<Logs />} />
-          <Route path="/users" element={<Users />} />
-          <Route path="/settings" element={<Settings />} />
-          <Route path="/employees" element={<Employee />} />
-          <Route path="/access-points" element={<AccessPoints />} />
-          <Route path="/reports" element={<Reports />} />
-          <Route path="*" index element={<Dashbord />} />
-        </Routes>
-      ) : (
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/help" element={<Help />} />
-          <Route path="/forgot-password" element={<ForgotPassword />} />
-          <Route path="*" element={<Login />} />
-        </Routes>
-      )}
-    </HashRouter>
+    <SocketDashboardWrapper>
+      <HashRouter>
+        {
+          //isLogged && user ? (
+          true ? (
+            <Routes>
+              <Route path="/" index element={<Dashbord />} />
+              <Route path="/help" element={<Help />} />
+              <Route path="/logs" element={<Logs />} />
+              <Route path="/users" element={<Users />} />
+              <Route path="/settings" element={<Settings />} />
+              <Route path="/employees" element={<Employee />} />
+              <Route path="/access-points" element={<AccessPoints />} />
+              <Route path="/reports" element={<Reports />} />
+              <Route path="*" index element={<Dashbord />} />
+            </Routes>
+          ) : (
+            <Routes>
+              <Route path="/login" element={<Login />} />
+              <Route path="/help" element={<Help />} />
+              <Route path="/forgot-password" element={<ForgotPassword />} />
+              <Route path="*" element={<Login />} />
+            </Routes>
+          )
+        }
+      </HashRouter>
+    </SocketDashboardWrapper>
   )
 }
