1b612a042fd67632fd59c49b72b41b3729410982
Axole Maranjana <82832027+Axolem@users.noreply.github.com>
2023-09-30 19:40:35 +0200

    add help #60

--- src/renderer/src/screens/Help.jsx
+++ src/renderer/src/screens/Help.jsx
@@ -5,12 +5,14 @@ import { Button } from 'primereact/button'
 import { ScrollPanel } from 'primereact/scrollpanel'
 import { TabView, TabPanel } from 'primereact/tabview'
 
-import { questions, mobile } from '../assets/helpdata.json'
+import { questions, tutorial } from '../assets/helpdata.json'
+import { API_URL } from '../utils/exports'
 
 //import frame1 from "../assets/guides/loginExample.html"
 
 const Help = () => {
-  const navigator = useNavigate()
+  const navigator = useNavigate();
+  const EMAIL = "catalystcrew@mts.co.za"
 
   return (
     <div
@@ -38,9 +40,24 @@ const Help = () => {
         <TabPanel header="Tutorials" leftIcon="pi pi-book mr-2">
           <ScrollPanel style={{ width: '96vw', height: '70vh' }}>
             <div className="flex flex-column gap-2">
-              <Panel collapsed header="How to login" toggleable></Panel>
-              <Panel collapsed header="How to reset password" toggleable></Panel>
-              <Panel collapsed header="How to turn of sound alerts" toggleable></Panel>
+              {tutorial.map((item) => <Panel
+                key={item.id}
+                collapsed
+                header={item.questionText}
+                toggleable
+              >
+                <div className='px-6'>
+                  <p>{item.answerText}</p>
+                  {
+                    item.media.map((media) => <>
+                      {media.type === "video" ?
+                        <video width={800} controls autoPlay src={`${API_URL}/static/${media.source}`} />
+                        :
+                        <image width={800} controls autoPlay src={`${API_URL}/static/${media.source}`} />}
+                    </>)
+                  }
+                </div>
+              </Panel>)}
             </div>
           </ScrollPanel>
         </TabPanel>
@@ -59,23 +76,54 @@ const Help = () => {
 
         <TabPanel header="Contact" leftIcon="pi pi-envelope mr-2">
           <ScrollPanel style={{ width: '96vw', height: '70vh' }}>
-            <div className="grid gap-3 m-auto p-5 justify-content-center">
-              <p className="m-0">
-                At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis
-                praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias
-                excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui
-                officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem
-                rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est
-                eligendi optio cumque nihil impedit quo minus.
-              </p>
-              <div className="col-item details-col">
-                <h3>Client Support</h3>
+            <div className="grid gap-3 m-auto p-5 justify-content-center flex flex-column">
+
+              <div>
+                <table style={{ margin: "auto" }} className='help-table'>
+                  <thead>
+                    <tr>
+                      <th>ID</th>
+                      <th>Name</th>
+                      <th>Devision</th>
+                      <th>Email</th>
+                      <th>Phone</th>
+                    </tr>
+                  </thead>
+                  <thead>
+                    <tr>
+                      <td>1</td>
+                      <td>Sam</td>
+                      <td>Rescue</td>
+                      <td><a href="mailto:sam@mail.com">sam@mail.com</a> </td>
+                      <td>011 234 5678</td>
+                    </tr>
+                    <tr>
+                      <td>2</td>
+                      <td>Nkosikhona Radebe</td>
+                      <td>Rescue (Lead)</td>
+                      <td><a href="mailto:nkosikhonar@mail.com">nkosikhonar@mail.com</a> </td>
+                      <td>011 432 5678</td>
+                    </tr>
+                    <tr>
+                      <td>3</td>
+                      <td>Yonela Charlie</td>
+                      <td>Emergrncy Personnel</td>
+                      <td><a href="mailto:yonelac@mail.com">yonelac@mail.com</a> </td>
+                      <td>011 342 5678</td>
+                    </tr>
+                  </thead>
+                </table>
+              </div>
+
+              <div className="col-item details-col text-center">
+                <h3>Email Support</h3>
                 <p>
-                  <a href="">
-                    <Button label="catalystcrew@gmail.com" rounded />
+                  <a href={`mailto:${EMAIL}`}>
+                    <Button label={EMAIL} rounded />
                   </a>
                 </p>
               </div>
+
             </div>
           </ScrollPanel>
         </TabPanel>
